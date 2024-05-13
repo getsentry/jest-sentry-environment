212a82b0109202ebc277d483d664728635c37537
Scott Cooper <scttcper@gmail.com>
2024-05-13 11:07:16 -0700

    formatting, sentry v8 peer dep

--- createEnvironment.js
+++ createEnvironment.js
@@ -1,14 +1,15 @@
-const Sentry = require("@sentry/node");
-const {nodeProfilingIntegration} = require("@sentry/profiling-node");
+const Sentry = require('@sentry/node');
+const {nodeProfilingIntegration} = require('@sentry/profiling-node');
 
 let DID_INIT_SENTRY = false;
 
 function isNotTransaction(span) {
-  return span.op !== "jest test";
+  return span.op !== 'jest test';
 }
 
-function createEnvironment({ baseEnvironment } = {}) {
-  const BaseEnvironment = baseEnvironment?.TestEnvironment || require("jest-environment-jsdom").TestEnvironment;
+function createEnvironment({baseEnvironment} = {}) {
+  const BaseEnvironment =
+    baseEnvironment?.TestEnvironment || require('jest-environment-jsdom').TestEnvironment;
 
   return class SentryEnvironment extends BaseEnvironment {
     getVmContextSpanStack = [];
@@ -18,7 +19,7 @@ function createEnvironment({ baseEnvironment } = {}) {
 
       const [config, context] = args;
 
-      this.options = config.projectConfig.testEnvironmentOptions?.sentryConfig
+      this.options = config.projectConfig.testEnvironmentOptions?.sentryConfig;
 
       if (
         !this.options ||
@@ -30,17 +31,19 @@ function createEnvironment({ baseEnvironment } = {}) {
         return;
       }
 
-      const { init } = this.options;
+      const {init} = this.options;
 
       this.Sentry = Sentry;
-      if(!DID_INIT_SENTRY) {
+      if (!DID_INIT_SENTRY) {
         // Ensure integration is an array as init is a user input
-        if(!Array.isArray(init.integrations)) {
+        if (!Array.isArray(init.integrations)) {
           init.integrations = [];
         }
 
         if (Sentry.autoDiscoverNodePerformanceMonitoringIntegrations) {
-          integrations.push(...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations())
+          integrations.push(
+            ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()
+          );
         }
 
         // Add profiling integration
@@ -48,10 +51,10 @@ function createEnvironment({ baseEnvironment } = {}) {
 
         this.Sentry.init(init);
         this.Sentry.setTags(this.options.tags || this.options.transactionOptions?.tags);
-        DID_INIT_SENTRY = true
+        DID_INIT_SENTRY = true;
       }
 
-      this.testPath = context.testPath.replace(process.cwd(), "");
+      this.testPath = context.testPath.replace(process.cwd(), '');
 
       this.runDescribe = new Map();
       this.testContainers = new Map();
@@ -66,7 +69,7 @@ function createEnvironment({ baseEnvironment } = {}) {
       }
 
       this.transaction = this.Sentry.startInactiveSpan({
-        op: "jest test suite",
+        op: 'jest test suite',
         description: this.testPath,
         name: this.testPath,
         forceTransaction: true,
@@ -75,12 +78,15 @@ function createEnvironment({ baseEnvironment } = {}) {
       this.global.Sentry = this.Sentry;
 
       this.Sentry.withActiveSpan(this.transaction, () => {
-        this.Sentry.startSpan({
-          op: "setup",
-          description: this.testPath,
-          }, async () => {
-          await super.setup();
-        });
+        this.Sentry.startSpan(
+          {
+            op: 'setup',
+            description: this.testPath,
+          },
+          async () => {
+            await super.setup();
+          }
+        );
       });
     }
 
@@ -91,21 +97,24 @@ function createEnvironment({ baseEnvironment } = {}) {
       }
 
       this.Sentry.withActiveSpan(this.transaction, () => {
-        this.Sentry.startSpan({
-          op: "teardown",
-          description: this.testPath,
-          }, async () => {
-          await super.teardown();
-          if (this.transaction) {
-            this.transaction.end();
+        this.Sentry.startSpan(
+          {
+            op: 'teardown',
+            description: this.testPath,
+          },
+          async () => {
+            await super.teardown();
+            if (this.transaction) {
+              this.transaction.end();
+            }
+            this.runDescribe = null;
+            this.testContainers = null;
+            this.tests = null;
+            this.hooks = null;
+            this.hub = null;
+            this.Sentry = null;
           }
-          this.runDescribe = null;
-          this.testContainers = null;
-          this.tests = null;
-          this.hooks = null;
-          this.hub = null;
-          this.Sentry = null;
-        });
+        );
       });
     }
 
@@ -113,7 +122,7 @@ function createEnvironment({ baseEnvironment } = {}) {
       if (this.transaction) {
         this.Sentry.withActiveSpan(this.transaction, () => {
           const getVmContextSpan = this.Sentry.startInactiveSpan({
-            op: "getVmContext",
+            op: 'getVmContext',
           });
           this.getVmContextSpanStack.push(getVmContextSpan);
         });
@@ -123,85 +132,81 @@ function createEnvironment({ baseEnvironment } = {}) {
 
     getName(parent) {
       if (!parent) {
-        return "";
+        return '';
       }
 
       // Ignore these for now as it adds a level of nesting and I'm not quite sure where it's even coming from
-      if (parent.name === "ROOT_DESCRIBE_BLOCK") {
-        return "";
+      if (parent.name === 'ROOT_DESCRIBE_BLOCK') {
+        return '';
       }
 
       const parentName = this.getName(parent.parent);
-      return `${parentName ? `${parentName} >` : ""} ${parent.name}`;
+      return `${parentName ? `${parentName} >` : ''} ${parent.name}`;
     }
 
-    getData({ name, ...event }) {
+    getData({name, ...event}) {
       switch (name) {
-        case "run_describe_start":
-        case "run_describe_finish":
-          if (name === "run_describe_finish") {
+        case 'run_describe_start':
+        case 'run_describe_finish':
+          if (name === 'run_describe_finish') {
             const span = this.getVmContextSpanStack.pop();
-            if(span){
+            if (span) {
               span.end();
             }
           }
 
           return {
-            op: "describe",
+            op: 'describe',
             obj: event.describeBlock,
             parentObj: event.describeBlock.parent,
             dataStore: this.runDescribe,
             parentStore: this.runDescribe,
           };
 
-        case "test_start":
-        case "test_done":
+        case 'test_start':
+        case 'test_done':
           return {
-            op: "test",
+            op: 'test',
             obj: event.test,
             parentObj: event.test.parent,
             dataStore: this.testContainers,
             parentStore: this.runDescribe,
-            beforeFinish: (span) => {
-              span.setStatus(
-                !event.test.errors.length ? "ok" : "internal_error"
-              );
+            beforeFinish: span => {
+              span.setStatus(!event.test.errors.length ? 'ok' : 'internal_error');
               return span;
             },
           };
 
-        case "test_fn_start":
-        case "test_fn_success":
-        case "test_fn_failure":
+        case 'test_fn_start':
+        case 'test_fn_success':
+        case 'test_fn_failure':
           return {
-            op: "test-fn",
+            op: 'test-fn',
             obj: event.test,
             parentObj: event.test,
             dataStore: this.tests,
             parentStore: this.testContainers,
-            beforeFinish: (span) => {
-              span.setStatus(
-                !event.test.errors.length ? "ok" : "internal_error"
-              );
+            beforeFinish: span => {
+              span.setStatus(!event.test.errors.length ? 'ok' : 'internal_error');
               return span;
             },
           };
 
-        case "hook_start":
+        case 'hook_start':
           return {
             obj: event.hook.parent,
             op: event.hook.type,
             dataStore: this.hooks,
           };
 
-        case "hook_success":
-        case "hook_failure":
+        case 'hook_success':
+        case 'hook_failure':
           return {
             obj: event.hook.parent,
             parentObj: event.test && event.test.parent,
             dataStore: this.hooks,
             parentStore: this.testContainers,
-            beforeFinish: (span) => {
+            beforeFinish: span => {
               const parent = this.testContainers.get(this.getName(event.test));
               if (parent && !Array.isArray(parent)) {
                 return parent.startChild(span);
@@ -212,15 +217,15 @@ function createEnvironment({ baseEnvironment } = {}) {
             },
           };
 
-        case "start_describe_definition":
-        case "finish_describe_definition":
-        case "add_test":
-        case "add_hook":
-        case "run_start":
-        case "run_finish":
-        case "test_todo":
-        case "setup":
-        case "teardown":
+        case 'start_describe_definition':
+        case 'finish_describe_definition':
+        case 'add_test':
+        case 'add_hook':
+        case 'run_start':
+        case 'run_finish':
+        case 'test_todo':
+        case 'setup':
+        case 'teardown':
           return null;
 
         default:
@@ -234,44 +239,37 @@ function createEnvironment({ baseEnvironment } = {}) {
       }
 
       const data = this.getData(event);
-      const { name } = event;
+      const {name} = event;
 
       if (!data) {
         return;
       }
 
-      const {
-        obj,
-        parentObj,
-        dataStore,
-        parentStore,
-        op,
-        description,
-        beforeFinish,
-      } = data;
+      const {obj, parentObj, dataStore, parentStore, op, description, beforeFinish} =
+        data;
 
       const testName = this.getName(obj);
 
-      if (name.includes("start")) {
+      if (name.includes('start')) {
         // Make this an option maybe
         if (!testName) {
           return;
         }
 
         const spans = [];
-        const spanProps = { op, description: description || testName };
+        const spanProps = {op, description: description || testName};
 
         // If we are starting a test, let's also make it a transaction so we can see our slowest tests
-        if (spanProps.op === "test") {
+        if (spanProps.op === 'test') {
           this.Sentry.withActiveSpan(this.transaction, () => {
             const testTransaction = this.Sentry.startInactiveSpan({
               ...spanProps,
-              op: "jest test",
+              op: 'jest test',
               name: spanProps.description,
               description: null,
-            })
+            });
             spans.push(testTransaction);
-          })
+          });
         }
 
         dataStore.set(testName, spans);
@@ -282,7 +280,7 @@ function createEnvironment({ baseEnvironment } = {}) {
       if (dataStore.has(testName)) {
         const spans = dataStore.get(testName);
 
-        if (name.includes("failure")) {
+        if (name.includes('failure')) {
           if (event.error) {
             this.Sentry.withActiveSpan(this.transaction, () => {
               this.Sentry.captureException(event.error);
@@ -290,11 +288,11 @@ function createEnvironment({ baseEnvironment } = {}) {
           }
         }
 
-        spans.forEach((span) => {
+        spans.forEach(span => {
           if (beforeFinish) {
             span = beforeFinish(span);
             if (!span) {
-              throw new Error("`beforeFinish()` needs to return a span");
+              throw new Error('`beforeFinish()` needs to return a span');
             }
           }
 
